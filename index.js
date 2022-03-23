949e05e99eb8c8f93884de5ebd5ddf3771d4082a
Xenia <kakirilyuk@gmail.com>
2022-03-22 21:27:43 -0400

    commit tree 04e68299cd92c724caa85d0a50e08bc1f7eb7df0

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = "surprise";
+
+function writeCards(names, event) {
+    let messageArray = [];
+    for (let i = 0; i < names.length; i++) {
+        let message =(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        messageArray.push(message);
+    }
+    return messageArray;
+}
+
+writeCards;
+
+
+countDown()
+
+"Expected countDown(4) to log 4 first"
+
+
+function countDown() {
+    let number = 10;
+    while (number >= 0) {
+        console.log(number--)
+    }
+}
